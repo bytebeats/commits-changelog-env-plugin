eec9d9591d93ce17a94619fdf16ea4ba6b919ca8
Chen Pan <happychinapc@gmail.com>
2022-02-16 17:26:37 +0800

    Update CommitsChangelogBuildContributor.java

--- src/main/java/me/bytebeats/jenkinsci/plugins/changelog/CommitsChangelogBuildContributor.java
+++ src/main/java/me/bytebeats/jenkinsci/plugins/changelog/CommitsChangelogBuildContributor.java
@@ -24,6 +24,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.IllegalFormatException;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by bytebeats on 2022/1/6 : 10:53
@@ -32,10 +33,15 @@ import java.util.List;
  */
 public class CommitsChangelogBuildContributor extends SimpleBuildWrapper {
     public static final String SCM_CHANGELOG = "SCM_CHANGELOG";
+    public static final String SCM_CHANGELOG_COUNT = "SCM_CHANGELOG_COUNT";
     public static final String LARK_WEBHOOK = "LARK_WEBHOOK";
     public static final String LARK_KEY = "LARK_KEY";
     public static final String USE_API_TOKEN_MODE = "USE_API_TOKEN_MODE";
+    public static final String AFFECTED_FILES_INCLUDED = "AFFECTED_FILES_INCLUDED";
     public static final String JENKINS_VISITOR = "JENKINS_VISITOR";
+    public static final String BUILD_START_TIME = "BUILD_START_TIME";
+    public static final String MAX_DISPLAYED_CHANGES = "MAX_DISPLAYED_CHANGES";
+    public static final String SCM_DATE_FORMAT = "SCM_DATE_FORMAT";
 
     private String entryFormat;
 
@@ -44,6 +50,10 @@ public class CommitsChangelogBuildContributor extends SimpleBuildWrapper {
     private String dateFormat;
 
     private boolean useApiTokenMode;
+
+    private boolean affectedFilesIncluded;
+
+    private String maxDisplayedChanges;
 
     private String jenkinsVisitor;
 
@@ -77,6 +87,16 @@ public class CommitsChangelogBuildContributor extends SimpleBuildWrapper {
     }
 
     @DataBoundSetter
+    public void setAffectedFilesIncluded(boolean affectedFilesIncluded) {
+        this.affectedFilesIncluded = affectedFilesIncluded;
+    }
+
+    @DataBoundSetter
+    public void setMaxDisplayedChanges(String maxDisplayedChanges) {
+        this.maxDisplayedChanges = maxDisplayedChanges;
+    }
+
+    @DataBoundSetter
     public void setJenkinsVisitor(String jenkinsVisitor) {
         this.jenkinsVisitor = jenkinsVisitor;
     }
@@ -106,7 +126,15 @@ public class CommitsChangelogBuildContributor extends SimpleBuildWrapper {
     public boolean isUseApiTokenMode() {
         return useApiTokenMode;
     }
-    
+
+    public boolean isAffectedFilesIncluded() {
+        return affectedFilesIncluded;
+    }
+
+    public String getMaxDisplayedChanges() {
+        return maxDisplayedChanges;
+    }
+
     public String getJenkinsVisitor() {
         return jenkinsVisitor;
     }
@@ -131,11 +159,27 @@ public class CommitsChangelogBuildContributor extends SimpleBuildWrapper {
             return;
         }
 
+        int maxChanges = 0;
+
+        try {
+            String nonNpe;
+            if (maxDisplayedChanges == null) {
+                nonNpe = "";
+            } else {
+                nonNpe = maxDisplayedChanges.trim();
+            }
+            maxChanges = Integer.parseInt(nonNpe);
+        } catch (NumberFormatException nfe) {
+            maxChanges = 30;
+        }
+
+        AtomicInteger counter = new AtomicInteger();
+
         try {
             if (build instanceof AbstractBuild<?, ?>) {
                 AbstractBuild<?, ?> abstractBuild = (AbstractBuild<?, ?>) build;
                 ChangeLogSet cs = abstractBuild.getChangeSet();
-                processChangeLogSet(sb, cs, df);
+                processChangeLogSet(sb, cs, df, maxChanges, counter);
             }
 
             try {
@@ -143,7 +187,7 @@ public class CommitsChangelogBuildContributor extends SimpleBuildWrapper {
                     WorkflowRun wfr = (WorkflowRun) build;
                     List<ChangeLogSet<? extends ChangeLogSet.Entry>> changeLogSets = wfr.getChangeSets();
                     for (ChangeLogSet<? extends ChangeLogSet.Entry> changeLogSet : changeLogSets) {
-                        processChangeLogSet(sb, changeLogSet, df);
+                        processChangeLogSet(sb, changeLogSet, df, maxChanges, counter);
                     }
                 }
             } catch (NoClassDefFoundError ncdr) {
@@ -155,12 +199,19 @@ public class CommitsChangelogBuildContributor extends SimpleBuildWrapper {
             return;
         }
 
+        context.env(BUILD_START_TIME, String.valueOf(build.getTimeInMillis()));
+
         String value = sb.toString();
         if (!"".equals(value)) {
             context.env(SCM_CHANGELOG, value);
         }
 
+        context.env(SCM_CHANGELOG_COUNT, String.valueOf(counter.get()));
+        context.env(MAX_DISPLAYED_CHANGES, String.valueOf(maxChanges));
+        context.env(SCM_DATE_FORMAT, Util.fixNull(dateFormat));
         context.env(USE_API_TOKEN_MODE, String.valueOf(useApiTokenMode));
+
+        context.env(AFFECTED_FILES_INCLUDED, String.valueOf(affectedFilesIncluded));
 
         if (null != jenkinsVisitor && !"".equals(jenkinsVisitor)) {
             context.env(JENKINS_VISITOR, Util.fixNull(jenkinsVisitor));
@@ -173,11 +224,20 @@ public class CommitsChangelogBuildContributor extends SimpleBuildWrapper {
         }
     }
 
-    private void processChangeLogSet(StringBuilder sb, ChangeLogSet cs, DateFormat df) {
+    private void processChangeLogSet(StringBuilder sb, ChangeLogSet cs, DateFormat df, int maxChanges, AtomicInteger counter) {
         for (Object o : cs) {
+            if (counter.get() > maxChanges) {
+                counter.incrementAndGet();
+                continue;
+            }
             ChangeLogSet.Entry e = (ChangeLogSet.Entry) o;
             sb.append(String.format(Util.fixNull(this.entryFormat), e.getAuthor(), e.getCommitId(), e.getMsg(), df.format(new Date(e.getTimestamp()))));
 
+            counter.incrementAndGet();
+
+            if (!affectedFilesIncluded) {
+                continue;
+            }
             try {
                 for (ChangeLogSet.AffectedFile file : e.getAffectedFiles()) {
                     sb.append(String.format(Util.fixNull(this.lineFormat), file.getEditType().getName(), file.getPath()));
@@ -233,6 +293,15 @@ public class CommitsChangelogBuildContributor extends SimpleBuildWrapper {
             }
         }
 
+        public FormValidation doCheckMaxDisplayedChanges(@QueryParameter String maxDisplayedChanges) {
+            try {
+                int maxDisplayedCount  = Integer.parseInt(maxDisplayedChanges.trim());
+                return FormValidation.ok(Messages.MaxDisplayedChanges_Sample(maxDisplayedCount));
+            } catch (IllegalArgumentException ex) {
+                return FormValidation.error(Messages.MaxDisplayedChanges_Error());
+            }
+        }
+
         public FormValidation doCheckJenkinsVisitor(@QueryParameter String jenkinsVisitor) {
             try {
                 String[] account = jenkinsVisitor.split(":");
@@ -255,6 +324,20 @@ public class CommitsChangelogBuildContributor extends SimpleBuildWrapper {
                 return FormValidation.ok(Messages.UseApiTokenMode_Sample(mode));
             } catch (Exception ex) {
                 return FormValidation.error(Messages.UseApiTokenMode_Error());
+            }
+        }
+
+        public FormValidation doCheckAffectedFilesIncluded(@QueryParameter boolean affectedFilesIncluded) {
+            try {
+                String mode;
+                if (affectedFilesIncluded) {
+                    mode = "Included";
+                } else {
+                    mode = "Excluded";
+                }
+                return FormValidation.ok(Messages.AffectedFilesIncluded_Sample(mode));
+            } catch (Exception ex) {
+                return FormValidation.error(Messages.AffectedFilesIncluded_Error());
             }
         }
     }
