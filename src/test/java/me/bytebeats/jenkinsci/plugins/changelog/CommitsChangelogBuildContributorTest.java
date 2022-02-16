9098dc517281537f85a9c81d91732d34d6f1e1c4
Chen Pan <happychinapc@gmail.com>
2022-02-16 17:26:03 +0800

    Update CommitsChangelogBuildContributorTest.java

--- src/test/java/me/bytebeats/jenkinsci/plugins/changelog/CommitsChangelogBuildContributorTest.java
+++ src/test/java/me/bytebeats/jenkinsci/plugins/changelog/CommitsChangelogBuildContributorTest.java
@@ -79,13 +79,19 @@ public class CommitsChangelogBuildContributorTest {
         public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener) throws InterruptedException, IOException {
             if (expected == null) {
                 assertNull("Not expecting " + CommitsChangelogBuildContributor.SCM_CHANGELOG + " in environment", run.getEnvironment(listener).get(CommitsChangelogBuildContributor.SCM_CHANGELOG));
+                assertNull("Not expecting " + CommitsChangelogBuildContributor.MAX_DISPLAYED_CHANGES + " in environment", run.getEnvironment(listener).get(CommitsChangelogBuildContributor.MAX_DISPLAYED_CHANGES));
+                assertNull("Not expecting " + CommitsChangelogBuildContributor.SCM_DATE_FORMAT + " in environment", run.getEnvironment(listener).get(CommitsChangelogBuildContributor.SCM_DATE_FORMAT));
                 assertNull("Not expecting " + CommitsChangelogBuildContributor.USE_API_TOKEN_MODE + " in environment", run.getEnvironment(listener).get(CommitsChangelogBuildContributor.USE_API_TOKEN_MODE));
+                assertNull("Not expecting " + CommitsChangelogBuildContributor.AFFECTED_FILES_INCLUDED + " in environment", run.getEnvironment(listener).get(CommitsChangelogBuildContributor.AFFECTED_FILES_INCLUDED));
                 assertNull("Not expecting " + CommitsChangelogBuildContributor.JENKINS_VISITOR + " in environment", run.getEnvironment(listener).get(CommitsChangelogBuildContributor.JENKINS_VISITOR));
                 assertNull("Not expecting " + CommitsChangelogBuildContributor.LARK_WEBHOOK + " in environment", run.getEnvironment(listener).get(CommitsChangelogBuildContributor.LARK_WEBHOOK));
                 assertNull("Not expecting " + CommitsChangelogBuildContributor.LARK_KEY + " in environment", run.getEnvironment(listener).get(CommitsChangelogBuildContributor.LARK_KEY));
             } else {
                 assertEquals("Expecting " + CommitsChangelogBuildContributor.SCM_CHANGELOG + " to contain string", expected, run.getEnvironment(listener).get(CommitsChangelogBuildContributor.SCM_CHANGELOG));
+                assertEquals("Expecting " + CommitsChangelogBuildContributor.MAX_DISPLAYED_CHANGES + " to contain string", expected, run.getEnvironment(listener).get(CommitsChangelogBuildContributor.MAX_DISPLAYED_CHANGES));
+                assertEquals("Expecting " + CommitsChangelogBuildContributor.SCM_DATE_FORMAT + " to contain string", expected, run.getEnvironment(listener).get(CommitsChangelogBuildContributor.SCM_DATE_FORMAT));
                 assertEquals("Expecting " + CommitsChangelogBuildContributor.USE_API_TOKEN_MODE + " to contain string", expected, run.getEnvironment(listener).get(CommitsChangelogBuildContributor.USE_API_TOKEN_MODE));
+                assertEquals("Expecting " + CommitsChangelogBuildContributor.AFFECTED_FILES_INCLUDED + " to contain string", expected, run.getEnvironment(listener).get(CommitsChangelogBuildContributor.AFFECTED_FILES_INCLUDED));
                 assertEquals("Expecting " + CommitsChangelogBuildContributor.JENKINS_VISITOR + " to contain string", expected, run.getEnvironment(listener).get(CommitsChangelogBuildContributor.JENKINS_VISITOR));
                 assertEquals("Expecting " + CommitsChangelogBuildContributor.LARK_WEBHOOK + " to contain string", expected, run.getEnvironment(listener).get(CommitsChangelogBuildContributor.LARK_WEBHOOK));
                 assertEquals("Expecting " + CommitsChangelogBuildContributor.LARK_KEY + " to contain string", expected, run.getEnvironment(listener).get(CommitsChangelogBuildContributor.LARK_KEY));
